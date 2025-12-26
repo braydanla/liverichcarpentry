ff826b1303909b822bd79bdc7e5fa415ac25e6dd
braydanla <57197726+braydanla@users.noreply.github.com>
2025-12-26 17:37:30 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   
-base: "/liverichcarpentry/",
+base: "/liverichcarpentry/index.html",
   
   server: {
     host: "::",
